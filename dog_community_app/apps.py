24bf6a0d607b402cf7506c548fec9b51a695a7c7
Lavinsh Parsai <lavinsh.parsai08@gmail.com>
2022-10-30 10:05:19 -0400

    Created App, updated Models and Settings

---
+++ dog_community_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DogCommunityAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dog_community_app'
---
+++ dog_community_app/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+class Breed(models.Model):
+    breed_id = models.IntegerField(primary_key=True)
+    breed_name = models.CharField(max_length=255)
+    breed_article = models.CharField(max_length=10000)
+    breed_image_path = models.CharField(max_length=1024)
+
+
+
+class Dogs(models.Model):
+    dog_id = models.IntegerField(primary_key=True)
+    breed = models.ForeignKey(Breed, on_delete=models.DO_NOTHING)
+    is_adopted = models.IntegerField()
+    dog_name = models.CharField(max_length=255)
+    dog_color = models.CharField(max_length=255)
+    dog_age = models.IntegerField()
+    is_disable = models.IntegerField()
+    disabilty = models.CharField(max_length=5000)
+    unique_identification = models.CharField(max_length=5000)
+    is_adoption_ready = models.IntegerField()
+
+
+class EventSubscriptions(models.Model):
+    subscription_id = models.IntegerField(primary_key=True)
+    event = models.ForeignKey('Events', on_delete=models.DO_NOTHING)
+    user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+
+
+class Events(models.Model):
+    event_id = models.IntegerField(primary_key=True)
+    event_location = models.CharField(max_length=5000)
+    event_time = models.DateTimeField()
+    event_duration = models.IntegerField()
+    event_capacity = models.IntegerField()
+    event_description = models.CharField(max_length=8000)
+
+
+class Reports(models.Model):
+    dog = models.ForeignKey(Dogs, on_delete=models.DO_NOTHING)
+    breed = models.ForeignKey(Breed, on_delete=models.DO_NOTHING)
+    reporter = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    last_known_location = models.CharField(max_length=10000)
+    category = models.CharField(max_length=255)
+
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    field_user_name = models.CharField(max_length=255)
+    user_address = models.CharField(max_length=5000)
+    user_contact = models.CharField(max_length=255)
+    user_email = models.CharField(max_length=255)
+
+class Admin(models.Model):
+    admin_id = models.IntegerField(primary_key=True)
+    admin_login_id = models.CharField(max_length=255)
+    admin_login_pass = models.CharField(max_length=255)
