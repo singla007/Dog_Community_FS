d555becc1a8d24e988d0e41c46c4ea624305a4b6
jaikumarkaushil <jaikumarkaushil@gmail.com>
2022-10-31 14:33:54 -0400

    Landing page create with views and templates

--- dog_community_app/views.py
+++ dog_community_app/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.shortcuts import (get_object_or_404,
+                              render,
+                              HttpResponseRedirect)
+
+from django.views.generic.edit import (CreateView, UpdateView, DeleteView, FormView)
+from django.views.generic.detail import DetailView
+
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+from django.views.generic.list import ListView
+
+from django.views import View
+
+# class MyView(View):
+#     def get(self, request):
+#         # <view logic>
+#         return HttpResponse('result')
+
+# class GeeksCreate(CreateView):
+#     # specify the model for create view
+#     model = GeeksModel
+#     # specify the fields to be displayed
+#     fields = ['title', 'description']
+#     def form_valid(self, form) -> HttpResponse:
+#         form.save()
+#         return HttpResponseRedirect("/")
+
+# class GeeksList(ListView):
+#     # specify the model for list view
+#     model = GeeksModel
+
+# class GeeksDetailView(DetailView):
+#     # specify the model to use
+#     model = GeeksModel
+
+# class GeeksUpdateView(UpdateView):
+#     # specify the model you want to use
+#     model = GeeksModel
+ 
+#     # specify the fields
+#     fields = [
+#         "title",
+#         "description"
+#     ]
+ 
+#     # can specify success url
+#     # url to redirect after successfully
+#     # updating details
+#     success_url ="/get/"
+
+# class GeeksDeleteView(DeleteView):
+#     # specify the model you want to use
+#     model = GeeksModel
+     
+#     # can specify success url
+#     # url to redirect after successfully
+#     # deleting object
+#     success_url ="/get/"
+
+# class GeeksFormView(FormView):
+#     # specify the Form you want to use
+#     form_class = GeeksForm
+     
+#     # specify name of template
+#     template_name = "geeks / geeksmodel_form.html"
+ 
+#     # can specify success url
+#     # url to redirect after successfully
+#     # updating details
+#     success_url ="/thanks/"
+
+# function based
+
+def home_view(request):
+    return render(request, "index.html")
+def aboutus_view(request):
+    return render(request, "aboutus.html")
