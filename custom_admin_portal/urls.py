81d76f9893cad7fb4232c1fc8598f85c42ecbe87
Yogesh Singla <eryogeshkumarsingla007@gmail.com>
2022-11-01 22:18:10 -0400

    adding code of admin panel

--- custom_admin_portal/urls.py
+++ custom_admin_portal/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', admin_login, name='admin_login'),
+    path('dashboard', dashboard, name='dashboard'),
 ]
--- custom_admin_portal/views.py
+++ custom_admin_portal/views.py
@@ -1,16 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from dog_community_app.models import Admin
+from django.contrib import messages
 
 # Create your views here.
 def admin_login(request):
     # Write login logic
-    login_id = request.POST.get("login_id")
+    admin_login_id = request.POST.get("login_id")
     password = request.POST.get("password")
-    print(login_id)
+    print(admin_login_id)
     print(password)
-    user_obj = Admin.objects.filter(login_id = login_id)
-    
+    user_obj = Admin.objects.filter(admin_login_id = admin_login_id)
+    # if not user_obj.exists():
+    #     messages.info(request, "Account not found")
+    #     return HttpResponse("<h1>Account not found</h1>")
+    # else:
+    #     return HttpResponse("<h1>Account found</h1>")
+
 
     return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
