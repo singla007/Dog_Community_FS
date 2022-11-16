99ac8987aad041925ef22d0c4c90408b328a6008
jaikumarkaushil <jaikumarkaushil@gmail.com>
2022-11-15 19:14:55 -0500

    milestone checkpoint

--- dog_community_app/admin.py
+++ dog_community_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Team, Breed, Dogs
+from .models import Team, Breed, Dogs, Reports,  ContactUs, Newsletter
 
 # Register your models here.
 admin.site.register(Team)
 admin.site.register(Breed)
 admin.site.register(Dogs)
+admin.site.register(Reports)
+admin.site.register(ContactUs)
+admin.site.register(Newsletter)
