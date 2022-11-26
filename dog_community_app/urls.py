bf5bde42c1b1faa893f0e0c793f3b0a36ac1edd7
jaikumarkaushil <jaikumarkaushil@gmail.com>
2022-11-25 19:54:16 -0500

    minor changes to UI

--- dog_community_app/urls.py
+++ dog_community_app/urls.py
@@ -7,9 +7,9 @@ from .views import *
 urlpatterns = [
     path('', home_view, name='main_home'),
     path('home', home_view, name='home'),
+    path('breed-info', breedinfo_view, name="breed-info"),
     path('aboutus', aboutus_view, name="aboutus"),
-    path('report_missing_dogs', report_missing_dogs_view, name="report_missing_dogs"),
-    path('report_stray_dogs', report_stray_dogs_view, name="report_stray_dogs"),
+    path('report_dogs/<type>', report_dogs_view, name="report_dogs"),
     path('adoption', adoption_view, name="adoption"),
     path('adoption/list', adoption_dog_list, name="adoption_dog_list")
 
